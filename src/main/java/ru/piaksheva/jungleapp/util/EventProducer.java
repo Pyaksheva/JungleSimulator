97e58db66aa48ea3b93f94aac308af8f0018622f
elena <ele-emelina@yandex.ru>
2023-08-24 02:33:19 +0300

    Add tiger activities

--- src/main/java/ru/piaksheva/jungleapp/util/EventProducer.java
+++ src/main/java/ru/piaksheva/jungleapp/util/EventProducer.java
@@ -11,10 +11,22 @@ public class EventProducer {
                 sleepEvent(tiger);
             } else if (eventNumber >= 40 && eventNumber < 60) {
                 moveEvent(tiger);
-            } else if (eventNumber >= 60 && eventNumber < 80) {
-                tigerEat(tiger);
-            } else if (eventNumber >= 80 && eventNumber <= 100) {
-                hunterAttack(tiger);
+            } else if (eventNumber >= 60 && eventNumber < 66) {
+                eatBoarEvent(tiger);
+            } else if (eventNumber >= 66 && eventNumber < 72) {
+                eatDeerEvent(tiger);
+            } else if (eventNumber >= 72 && eventNumber < 78) {
+                huntShareDeerEvent(tiger);
+            } else if (eventNumber >= 78 && eventNumber < 81) {
+                growlEvent(tiger);
+            } else if (eventNumber >= 81 && eventNumber < 88) {
+                defendTerritoryEvent(tiger);
+            } else if (eventNumber >= 88 && eventNumber < 90) {
+                lickWoolEvent(tiger);
+            } else if (eventNumber >= 90 && eventNumber < 96) {
+                reproduceEvent(tiger);
+            } else if (eventNumber >= 96 && eventNumber <= 100) {
+                hunterAttackEvent(tiger);
             }
             try {
                 Thread.sleep(1500);
@@ -31,7 +43,7 @@ public class EventProducer {
         if (energy > 100) {
             energy = 100;
         }
-        System.out.println("Тигр поспал днем! " + " + " + (energy - tiger.getEnergy()) + " энергии. Текущая энергия: " + energy);
+        System.out.println("Тигр поспал! " + " + " + (energy - tiger.getEnergy()) + " энергии. Текущая энергия: " + energy);
         tiger.setEnergy(energy);
         checkEnergy(tiger);
     }
@@ -45,26 +57,96 @@ public class EventProducer {
         checkEnergy(tiger);
     }
 
-    private void tigerEat(Tiger tiger) {
+    private void eatBoarEvent(Tiger tiger) {
         int energy = tiger.getEnergy();
         int health = tiger.getHealth();
-        energy = energy - 15;
+        energy = energy - 12;
         energy = normalizeEnergy(energy);
         health = health + (int) (tiger.getPreySizeRate() * 4);
         health = normalizeHealth(health);
         tiger.setEnergy(energy);
         tiger.setHealth(health);
         checkEnergy(tiger);
-        System.out.println("Тигр съел дикого кабана! Текущая энергия: " + tiger.getEnergy() + "Текущее здоровье: " + tiger.getHealth());
+        System.out.println("Тигр съел дикого кабана! Текущая энергия: " + tiger.getEnergy() + " Текущее здоровье: " + tiger.getHealth());
     }
 
-    private void hunterAttack(Tiger tiger) {
+    private void eatDeerEvent(Tiger tiger) {
+        int energy = tiger.getEnergy();
+        int health = tiger.getHealth();
+        energy = energy - 15;
+        energy = normalizeEnergy(energy);
+        health = health + (int) (tiger.getPreySizeRate() * 6);
+        health = normalizeHealth(health);
+        tiger.setEnergy(energy);
+        tiger.setHealth(health);
+        checkEnergy(tiger);
+        System.out.println("Тигр съел оленя! Текущая энергия: " + tiger.getEnergy() + " Текущее здоровье: " + tiger.getHealth());
+    }
+
+    private void huntShareDeerEvent(Tiger tiger) {
+        int energy = tiger.getEnergy();
+        int health = tiger.getHealth();
+        energy = energy - 15;
+        energy = normalizeEnergy(energy);
+        health = health + (int) ((tiger.getPreySizeRate() / 2) * 6);
+        health = normalizeHealth(health);
+        checkEnergy(tiger);
+        tiger.setEnergy(energy);
+        tiger.setHealth(health);
+        System.out.println("Тигр поймал оленя и любезно разделил добычу с тигрицей! Текущий уровень энергии: " + tiger.getEnergy() + " Текущее здоровье: " + tiger.getHealth());
+    }
+
+    private void hunterAttackEvent(Tiger tiger) {
         int health = tiger.getHealth();
         health = health - 30;
         health = normalizeHealth(health);
         tiger.setHealth(health);
         checkEnergy(tiger);
         System.out.println("На тигра напал браконьер! Текущее здоровье: " + tiger.getHealth());
+    }
+
+    private void growlEvent(Tiger tiger) {
+        int energy = tiger.getEnergy();
+        energy = energy - 5;
+        energy = normalizeEnergy(energy);
+        tiger.setEnergy(energy);
+        checkEnergy(tiger);
+        System.out.println("Тигр рычит!!! Текущая энергия: " + tiger.getEnergy());
+    }
+
+    private void defendTerritoryEvent(Tiger tiger) {
+        int energy = tiger.getEnergy();
+        int health = tiger.getHealth();
+        energy = energy - 30;
+        health = health - 15;
+        energy = normalizeEnergy(energy);
+        health = normalizeHealth(health);
+        tiger.setEnergy(energy);
+        tiger.setHealth(health);
+        checkEnergy(tiger);
+        System.out.println("Тигр защищает свою территорию от чужака - другого тигра! Текущая энергия: " + tiger.getEnergy() + " Текущее здоровье: " + tiger.getHealth());
+    }
+
+    private void lickWoolEvent(Tiger tiger) {
+        int energy = tiger.getEnergy();
+        int health = tiger.getHealth();
+        energy = energy - 3;
+        health = health + 5;
+        energy = normalizeEnergy(energy);
+        health = normalizeHealth(health);
+        tiger.setEnergy(energy);
+        tiger.setHealth(health);
+        checkEnergy(tiger);
+        System.out.println("Тигр вылизывает шерсть! Текущая энергия: " + tiger.getEnergy() + " Текущее здоровье: " + tiger.getHealth());
+    }
+
+    private void reproduceEvent(Tiger tiger) {
+        int energy = tiger.getEnergy();
+        energy = energy - 7;
+        energy = normalizeEnergy(energy);
+        tiger.setEnergy(energy);
+        checkEnergy(tiger);
+        System.out.println("Тигр спаривается с тигрицей! Текущая энергия: " + tiger.getEnergy());
     }
 
     private boolean checkStatus(Tiger tiger) {
